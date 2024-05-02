7bc74eaae36025c68e7e0319dc0af62f1ade0259
vladokuskov <vladokuskov@gmail.com>
2024-05-02 11:22:20 +0300

    MDLY-9 Implement basic skeleton for application: fixed warning - Warning: Extra attributes from the server

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="light">
+    <html lang="en" data-theme="light" suppressHydrationWarning={true}>
       <body
         className={`${roboto.variable} ${inter.variable}`}
         suppressHydrationWarning={true}
