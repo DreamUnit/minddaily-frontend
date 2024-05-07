73fe0fd51b30354fc44f30d0a0ab78d9f2a1cdc8
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2024-05-07 07:32:10 +0100

    Fixed linting issues!

--- src/@types/__graphqlTypes__/graphql.ts
+++ src/@types/__graphqlTypes__/graphql.ts
@@ -2,19 +2,32 @@
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
-export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+export type MakeEmpty<
+  T extends { [key: string]: unknown },
+  K extends keyof T,
+> = { [_ in K]?: never };
+export type Incremental<T> =
+  | T
+  | {
+      [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never;
+    };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: { input: string; output: string; }
-  String: { input: string; output: string; }
-  Boolean: { input: boolean; output: boolean; }
-  Int: { input: number; output: number; }
-  Float: { input: number; output: number; }
-  DateTime: { input: any; output: any; }
+  ID: { input: string; output: string };
+  String: { input: string; output: string };
+  Boolean: { input: boolean; output: boolean };
+  Int: { input: number; output: number };
+  Float: { input: number; output: number };
+  DateTime: { input: any; output: any };
 };
 
 export type Diary = {
@@ -73,12 +86,10 @@ export type Mutation = {
   updateUser?: Maybe<ReadUserResponse>;
 };
 
-
 export type MutationCreateDiaryArgs = {
   title: Scalars['String']['input'];
   userId: Scalars['ID']['input'];
 };
-
 
 export type MutationCreateDiaryNoteArgs = {
   diaryId: Scalars['ID']['input'];
@@ -86,41 +97,34 @@ export type MutationCreateDiaryNoteArgs = {
   title: Scalars['String']['input'];
 };
 
-
 export type MutationCreateUserArgs = {
   authUserId: Scalars['ID']['input'];
   email: Scalars['String']['input'];
   name: Scalars['String']['input'];
 };
 
-
 export type MutationDeleteDiaryArgs = {
   id: Scalars['ID']['input'];
 };
-
 
 export type MutationDeleteDiaryNoteArgs = {
   id: Scalars['ID']['input'];
 };
 
-
 export type MutationDeleteUserArgs = {
   id: Scalars['ID']['input'];
 };
-
 
 export type MutationUpdateDiaryArgs = {
   id: Scalars['ID']['input'];
   title?: InputMaybe<Scalars['String']['input']>;
 };
 
-
 export type MutationUpdateDiaryNoteArgs = {
   id: Scalars['ID']['input'];
   text?: InputMaybe<Scalars['String']['input']>;
   title: Scalars['String']['input'];
 };
-
 
 export type MutationUpdateUserArgs = {
   active?: InputMaybe<Scalars['Boolean']['input']>;
@@ -142,33 +146,27 @@ export type Query = {
   readUsers?: Maybe<ReadUsersResponse>;
 };
 
-
 export type QueryReadDiariesArgs = {
   skip: Scalars['Int']['input'];
   take: Scalars['Int']['input'];
 };
 
-
 export type QueryReadDiaryByIdArgs = {
   id: Scalars['ID']['input'];
 };
 
-
 export type QueryReadDiaryNoteByIdArgs = {
   id: Scalars['ID']['input'];
 };
-
 
 export type QueryReadDiaryNotesArgs = {
   skip: Scalars['Int']['input'];
   take: Scalars['Int']['input'];
 };
 
-
 export type QueryReadUserByIdArgs = {
   id: Scalars['ID']['input'];
 };
-
 
 export type QueryReadUsersArgs = {
   skip: Scalars['Int']['input'];
@@ -254,8 +252,193 @@ export type ReadDiariesQueryVariables = Exact<{
   skip: Scalars['Int']['input'];
 }>;
 
+export type ReadDiariesQuery = {
+  __typename?: 'Query';
+  readDiaries?: {
+    __typename?: 'ReadDiariesResponse';
+    code: number;
+    success: boolean;
+    message?: string | null;
+    count?: number | null;
+    data?: Array<{
+      __typename?: 'Diary';
+      createdDate: any;
+      updatedDate?: any | null;
+      deletedDate?: any | null;
+      version: number;
+      id: string;
+      userId: string;
+      title: string;
+      notes: Array<{
+        __typename?: 'DiaryNote';
+        createdDate: any;
+        updatedDate?: any | null;
+        deletedDate?: any | null;
+        version: number;
+        id: string;
+        diaryId: string;
+        title: string;
+        text?: string | null;
+        images: Array<{
+          __typename?: 'Image';
+          url: string;
+          title?: string | null;
+        } | null>;
+      } | null>;
+    } | null> | null;
+  } | null;
+};
 
-export type ReadDiariesQuery = { __typename?: 'Query', readDiaries?: { __typename?: 'ReadDiariesResponse', code: number, success: boolean, message?: string | null, count?: number | null, data?: Array<{ __typename?: 'Diary', createdDate: any, updatedDate?: any | null, deletedDate?: any | null, version: number, id: string, userId: string, title: string, notes: Array<{ __typename?: 'DiaryNote', createdDate: any, updatedDate?: any | null, deletedDate?: any | null, version: number, id: string, diaryId: string, title: string, text?: string | null, images: Array<{ __typename?: 'Image', url: string, title?: string | null } | null> } | null> } | null> | null } | null };
-
-
-export const ReadDiariesDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"ReadDiaries"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"take"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"Int"}}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"skip"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"Int"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"readDiaries"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"take"},"value":{"kind":"Variable","name":{"kind":"Name","value":"take"}}},{"kind":"Argument","name":{"kind":"Name","value":"skip"},"value":{"kind":"Variable","name":{"kind":"Name","value":"skip"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"code"}},{"kind":"Field","name":{"kind":"Name","value":"success"}},{"kind":"Field","name":{"kind":"Name","value":"message"}},{"kind":"Field","name":{"kind":"Name","value":"data"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"createdDate"}},{"kind":"Field","name":{"kind":"Name","value":"updatedDate"}},{"kind":"Field","name":{"kind":"Name","value":"deletedDate"}},{"kind":"Field","name":{"kind":"Name","value":"version"}},{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"userId"}},{"kind":"Field","name":{"kind":"Name","value":"title"}},{"kind":"Field","name":{"kind":"Name","value":"notes"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"createdDate"}},{"kind":"Field","name":{"kind":"Name","value":"updatedDate"}},{"kind":"Field","name":{"kind":"Name","value":"deletedDate"}},{"kind":"Field","name":{"kind":"Name","value":"version"}},{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"diaryId"}},{"kind":"Field","name":{"kind":"Name","value":"title"}},{"kind":"Field","name":{"kind":"Name","value":"text"}},{"kind":"Field","name":{"kind":"Name","value":"images"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"url"}},{"kind":"Field","name":{"kind":"Name","value":"title"}}]}}]}}]}},{"kind":"Field","name":{"kind":"Name","value":"count"}}]}}]}}]} as unknown as DocumentNode<ReadDiariesQuery, ReadDiariesQueryVariables>;
+export const ReadDiariesDocument = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'query',
+      name: { kind: 'Name', value: 'ReadDiaries' },
+      variableDefinitions: [
+        {
+          kind: 'VariableDefinition',
+          variable: { kind: 'Variable', name: { kind: 'Name', value: 'take' } },
+          type: {
+            kind: 'NonNullType',
+            type: { kind: 'NamedType', name: { kind: 'Name', value: 'Int' } },
+          },
+        },
+        {
+          kind: 'VariableDefinition',
+          variable: { kind: 'Variable', name: { kind: 'Name', value: 'skip' } },
+          type: {
+            kind: 'NonNullType',
+            type: { kind: 'NamedType', name: { kind: 'Name', value: 'Int' } },
+          },
+        },
+      ],
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'readDiaries' },
+            arguments: [
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'take' },
+                value: {
+                  kind: 'Variable',
+                  name: { kind: 'Name', value: 'take' },
+                },
+              },
+              {
+                kind: 'Argument',
+                name: { kind: 'Name', value: 'skip' },
+                value: {
+                  kind: 'Variable',
+                  name: { kind: 'Name', value: 'skip' },
+                },
+              },
+            ],
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                { kind: 'Field', name: { kind: 'Name', value: 'code' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'success' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'message' } },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'data' },
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      {
+                        kind: 'Field',
+                        name: { kind: 'Name', value: 'createdDate' },
+                      },
+                      {
+                        kind: 'Field',
+                        name: { kind: 'Name', value: 'updatedDate' },
+                      },
+                      {
+                        kind: 'Field',
+                        name: { kind: 'Name', value: 'deletedDate' },
+                      },
+                      {
+                        kind: 'Field',
+                        name: { kind: 'Name', value: 'version' },
+                      },
+                      { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                      {
+                        kind: 'Field',
+                        name: { kind: 'Name', value: 'userId' },
+                      },
+                      { kind: 'Field', name: { kind: 'Name', value: 'title' } },
+                      {
+                        kind: 'Field',
+                        name: { kind: 'Name', value: 'notes' },
+                        selectionSet: {
+                          kind: 'SelectionSet',
+                          selections: [
+                            {
+                              kind: 'Field',
+                              name: { kind: 'Name', value: 'createdDate' },
+                            },
+                            {
+                              kind: 'Field',
+                              name: { kind: 'Name', value: 'updatedDate' },
+                            },
+                            {
+                              kind: 'Field',
+                              name: { kind: 'Name', value: 'deletedDate' },
+                            },
+                            {
+                              kind: 'Field',
+                              name: { kind: 'Name', value: 'version' },
+                            },
+                            {
+                              kind: 'Field',
+                              name: { kind: 'Name', value: 'id' },
+                            },
+                            {
+                              kind: 'Field',
+                              name: { kind: 'Name', value: 'diaryId' },
+                            },
+                            {
+                              kind: 'Field',
+                              name: { kind: 'Name', value: 'title' },
+                            },
+                            {
+                              kind: 'Field',
+                              name: { kind: 'Name', value: 'text' },
+                            },
+                            {
+                              kind: 'Field',
+                              name: { kind: 'Name', value: 'images' },
+                              selectionSet: {
+                                kind: 'SelectionSet',
+                                selections: [
+                                  {
+                                    kind: 'Field',
+                                    name: { kind: 'Name', value: 'url' },
+                                  },
+                                  {
+                                    kind: 'Field',
+                                    name: { kind: 'Name', value: 'title' },
+                                  },
+                                ],
+                              },
+                            },
+                          ],
+                        },
+                      },
+                    ],
+                  },
+                },
+                { kind: 'Field', name: { kind: 'Name', value: 'count' } },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+} as unknown as DocumentNode<ReadDiariesQuery, ReadDiariesQueryVariables>;
